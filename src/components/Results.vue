f0a89c41b3f383c50987b2df4b0c65443dbc7081
mendi18 <mmendell770@gmail.com>
2021-06-21 17:14:27 -0300

    projeto finalizado

---
+++ src/components/Results.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <h3>Reaction Time {{ results }} ms</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    props:[
+        'results'
+    ]
+}
+</script>
+
+<style>
+
+</style>
